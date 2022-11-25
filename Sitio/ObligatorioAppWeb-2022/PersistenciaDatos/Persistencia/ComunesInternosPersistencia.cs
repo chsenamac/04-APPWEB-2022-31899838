86d9ba33b4d57ba70d10d8e9ad3a8ddd985e9a6c
Christiams Sena <csena.machado@outlook.com>
2022-11-25 11:20:25 -0300

    Initial commit

---
+++ Sitio/ObligatorioAppWeb-2022/PersistenciaDatos/Persistencia/ComunesInternosPersistencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Entidades;
+
+namespace Persistencia
+{
+    internal class ComunesInternosPersistencia
+    {
+        private string strConexion = "Data Source=.; Initial Catalog=ObligatorioAppWeb2022; integrated Security=true";
+
+        public string StringConexion
+        {
+            get { return strConexion; }
+        }
+
+    }
+}
